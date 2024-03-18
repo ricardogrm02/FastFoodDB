97549c4446d16a233ba61f60183bc12804a6af4b
randyydoo <randyydoo@gmail.com>
2024-03-18 15:14:48 -0700

    updated schema

--- FoodDBCode/model/review.js
+++ FoodDBCode/model/review.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const reviewSchema = new mongoose.Schema({
-  rating: Number,
-  title: String,
-  description: String,
-  userID: Number,
-},{
-    collection: 'Customer'
-});
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: { Number, required: true },
+    title: { String, required: true },
+    description: { String, required: true },
+    userID: { Number, unique: true },
+  },
+  {
+    collection: "Customer",
+  },
+);
 
 const Review = mongoose.model("review", reviewSchema);
 
