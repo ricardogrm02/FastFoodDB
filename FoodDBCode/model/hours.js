fc6bac3318059a6cdd7f62777f761b0db3c0a299
marsxnow <raxelportiz@gmail.com>
2024-03-22 17:08:51 -0700

    Updating hours schema

--- FoodDBCode/model/hours.js
+++ FoodDBCode/model/hours.js
@@ -1,7 +1,9 @@
 const moongoes = require("mongoose");
 
 const hoursSchema = new moongoes.Schema({
+  date: { type: Date, required: true },
   hours: { type: Number, required: true },
+  overtime: { type: Number, required: true },
 });
 
 const Hours = new moongoes.model("Hours", hoursSchema);
